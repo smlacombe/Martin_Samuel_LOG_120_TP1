abfe012b4add4d2fb9e5cf2fa2c1968bbcbf22a6
authchir <martin.desharnais@gmail.com>
2011-10-08 14:33:31 -0400

    Ajout de commentaires et mise aux normes de codage de la classe ets.log120.tp1.Shape.

--- SwingApplication/src/ets/log120/tp1/Shape.java
+++ SwingApplication/src/ets/log120/tp1/Shape.java
@@ -1,34 +1,45 @@
 package ets.log120.tp1;
 
-import java.awt.Color;
-import java.awt.Graphics;
-import ca.etsmtl.log.util.IDLogger;
-
+/**
+ * Forme qui pourra être affichée dans un environement graphique.
+ * 
+ * @author Samuel Milette-Lacombe & Martin Desharnais
+ */
 public abstract class Shape {
-	
-	private Color color;
-	protected int sequenceNumber;
-	protected int x;
-	protected int y;
-		
-	//////////////////////////////////////////////////
-	// Constructor(s)
-	//////////////////////////////////////////////////
-	
-	public Shape (Color color, int sequenceNumber, int x, int y) {
+
+	// ////////////////////////////////////////////////
+	// Constructeurs(s)
+	// ////////////////////////////////////////////////
+
+	/**
+	 * Construit une forme avec une couleur, un numéro d'identification et ses
+	 * coordonnées.
+	 */
+	public Shape(java.awt.Color color, int sequenceNumber, int x, int y) {
 		this.color = color;
 		this.sequenceNumber = sequenceNumber;
 		this.x = x;
 		this.y = y;
-		IDLogger.getInstance().logID(sequenceNumber);		
+		ca.etsmtl.log.util.IDLogger.getInstance().logID(sequenceNumber);
 	}
-	
-	//////////////////////////////////////////////////
-	// Method(s)
-	//////////////////////////////////////////////////
-	
-	public void draw(Graphics g) {
+
+	// ////////////////////////////////////////////////
+	// Méthode(s)
+	// ////////////////////////////////////////////////
+
+	/**
+	 * Dessine la forme sur l'environement graphique reçu en paramètre.
+	 */
+	public void draw(java.awt.Graphics g) {
 		g.setColor(color);
 	}
 	
+	////////////////////////////////////////////////////
+	// Attribut(s)
+	////////////////////////////////////////////////////
+
+	private java.awt.Color color;
+	protected int sequenceNumber;
+	protected int x;
+	protected int y;
 }
