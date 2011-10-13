3004c5c9d03e145bf2f1c7ad031f1fe0154e7ca2
authchir <martin.desharnais@gmail.com>
2011-10-12 22:08:22 -0400

    Mise aux normes de programmation du code source.

--- SwingApplication/src/ets/log120/tp1/Circle.java
+++ SwingApplication/src/ets/log120/tp1/Circle.java
@@ -1,11 +1,7 @@
 package ets.log120.tp1;
 
-import java.awt.Color;
-import java.awt.Graphics;
-
 public class Circle extends Oval {
-	
 	public Circle(int sequenceNumber, int x, int y, int radius) {
-		super(Color.YELLOW, sequenceNumber, x, y, radius, radius);
+		super(java.awt.Color.YELLOW, sequenceNumber, x, y, radius, radius);
 	}
 }
--- SwingApplication/src/ets/log120/tp1/Square.java
+++ SwingApplication/src/ets/log120/tp1/Square.java
@@ -1,11 +1,7 @@
 package ets.log120.tp1;
 
-import java.awt.Color;
-import java.awt.Graphics;
-
 public class Square extends Rectangle {
-	
 	public Square(int sequenceNumber, int x1, int y1, int x2, int y2) {
-		super(Color.ORANGE, sequenceNumber, x1, y1, x2, y2);
+		super(java.awt.Color.ORANGE, sequenceNumber, x1, y1, x2, y2);
 	}
 }
