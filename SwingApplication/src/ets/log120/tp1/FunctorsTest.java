102061ba9b7c3608c5572f6c7abcc5f9c88f0796
authchir <martin.desharnais@gmail.com>
2011-10-21 14:53:03 -0400

    Ajout des test du tri par ordre séquentiel inverse.

--- SwingApplication/src/ets/log120/tp1/FunctorsTest.java
+++ SwingApplication/src/ets/log120/tp1/FunctorsTest.java
@@ -45,4 +45,24 @@ public class FunctorsTest {
 		assertEquals(i.next(), rectangle);
 		assertEquals(i.next(), oval);
 	}
+	
+	@Test
+	public void testSequenceNumberDescending() {
+		Iterator<Shape> i = l.iterator();
+		
+		assertEquals(i.next(), square);
+		assertEquals(i.next(), rectangle);
+		assertEquals(i.next(), line);
+		assertEquals(i.next(), circle);
+		assertEquals(i.next(), oval);
+		
+		l.sort(new Functors.Not(new Functors.SequenceNumberAscending()));
+		
+		i = l.iterator();
+		assertEquals(i.next(), oval);
+		assertEquals(i.next(), rectangle);
+		assertEquals(i.next(), line);
+		assertEquals(i.next(), square);
+		assertEquals(i.next(), circle);
+	}
 }
